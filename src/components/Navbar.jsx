29c578cf18526258d3e553cb7ad1b3a7c302c53a
adbi-hk <biaddiaelhak@gmail.com>
2025-03-10 14:48:15 +0100

    1:40

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,36 +7,85 @@ import { MdKeyboardArrowDown } from 'react-icons/md'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import avatar from '../data/avatar.jpg'
-import { Cart , Chat , Notification , userProfile } from '.'
+import { Cart , Chat , Notification , UserProfile, userProfile } from '.'
 import { useStateContext } from '../contexts/ContextProvider' 
 
 const NavButton = ({title ,custoFunc , icon , color , dotColor }) => (
   <TooltipComponent content={title} position='BottomCenter'>
-      <button type='button' onClick={custoFunc} style={{color}} className='relative text-xl rounded-full p3 hover:bg-light-gray'>
-           <span style={{background:dotColor}} className='absolute inline-flex rounded-fullh-2 w-2 right-2 top-2'>
-              {icon}
-           </span>
+      <button type='button' onClick={custoFunc} style={{color}} className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
+           <span style={{background:dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
+           />
+           {icon}
+           
       </button>
   </TooltipComponent>
 ) 
 const Navbar = () => {
-  const {activeMenu , setActiveMenu} = useStateContext ();
+  const {screenSize , setScreenSize , handleClick , activeMenu , setActiveMenu , isClicked , setIsClicked } = useStateContext ();
+  useEffect(()=>{
+  const handleResize = () => setScreenSize(window.innerWidth); window.addEventListener('resize',handleResize); handleResize()
+  return() =>window.removeEventListener('resize',handleResize)}, []);
+  useEffect(()=>{ if (screenSize <= 700){setActiveMenu(false)}else {setActiveMenu(true)} }, [screenSize])
   return (
-    <div className='flex justify-between p-2 md:mx-6 relative'>
+    <div className='flex justify-between p-2 md:ml-6 md:mr-6 relative'>
+      
         <NavButton title="menu"
                    custoFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} 
                    color="blue" 
                    icon={<AiOutlineMenu/>}/>
-        <div className='flex'>
-          <NavButton title="Cart" 
-                     custoFunc={() => handleClick('cart')} 
-                     color="blue" 
-                     icon={<FiShoppingCart/>}
-          />
+        
+        <div className=" flex">
+                
+                <NavButton title="Cart" 
+                          custoFunc={() => handleClick('cart')} 
+                          color="blue" 
+                          icon={<FiShoppingCart/>}
+                />
+         
+          
+          
+          
+                <NavButton title="Chat" 
+                          dotColor="#03C9D7"
+                          custoFunc={() => handleClick('chat')} 
+                          color="blue" 
+                          icon={<BsChatLeft/>}
+                />
+                 
+          
+          
+                <NavButton title="Notifications" 
+                           dotColor="#03C9D7"
+                           custoFunc={() => handleClick('notification')} 
+                           color="blue" 
+                           icon={<RiNotification3Line/>}
+                />
+          
+          
+          
+                <TooltipComponent content="Profile" position='BottomCenter'>
+            
+                    <div className='flex items-center gap-2 curser-pointer p-1 hover:bg-light-gray rounded-lg'
+                        onClick={()=>handleClick("userProfile")}>
+                          <img src={avatar} 
+                              className='rounded-full w-8 h-8'
+                          />
+                          <p>
+                              <span className='text-gray-400 text-14'>Hi ,</span>  {'  '}
+                              <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+                          </p>   
+                          <MdKeyboardArrowDown className='text-gray-400 text-14' />
+                    </div>
+                </TooltipComponent>
+                
+                {isClicked.cart && <Cart/>}
+                {isClicked.chat && <Chat/>}
+                {isClicked.notification && <Notification/>}
+                {isClicked.userProfile && <UserProfile/>}
         </div>
     </div>
     
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -10,12 +10,21 @@ const initialState = {
 }
 
 export const ContextProvider = ({children}) =>{
-    const [activeMenu,setActiveMenu] = useState(true)
+    const [activeMenu,setActiveMenu] = useState(true);
+    const [isClicked,setIsClicked] = useState(initialState);
+    const handleClick = (clicked) => {setIsClicked({...initialState , [clicked]:true})}
+    const [screenSize,setScreenSize] =useState (undefined)
+
     return (
         <StateContext.Provider
         value={
             { activeMenu : activeMenu,
               setActiveMenu : setActiveMenu,
+              isClicked : isClicked ,
+              setIsClicked : setIsClicked ,
+              handleClick ,
+              screenSize ,
+              setScreenSize,
         }}
         >
          {children}
